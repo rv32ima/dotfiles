7d4d264e29358968e73bfdefd245aaf015c392a5
Ellie Ford <me@ellie.fm>
2025-09-12 20:08:27 -0700

    aaaaaa

--- nix/machines/fadeoutz/default.nix
+++ nix/machines/fadeoutz/default.nix
@@ -17,11 +17,11 @@ in
       loader.grub.efiSupport = true;
       loader.grub.mirroredBoots = [
         {
-          devices = [ "/dev/disk/by-id/scsi-364cd98f0bbd0f40030574fa2831b8ed7-part1" ];
+          devices = [ "nodev" ];
           path = "/boot1";
         }
         {
-          devices = [ "/dev/disk/by-id/scsi-364cd98f0bbd0f40030574fa3852291f2-part1" ];
+          devices = [ "nodev" ];
           path = "/boot2";
         }
       ];
