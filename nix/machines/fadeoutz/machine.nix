fea1fe012a92d3a40e47e33bdb3a3c4c38e33cbf
Ellie Ford <me@ellie.fm>
2025-09-12 20:48:57 -0700

    add more stuff

--- nix/machines/fadeoutz/machine.nix
+++ nix/machines/fadeoutz/machine.nix
@@ -3,5 +3,6 @@
   hostName = "fadeoutz";
   stateVersion = "25.05";
   system = "x86_64-linux";
+  primaryUser = "ellie";
   isRemote = false;
 }
--- nix/users/ellie.nix
+++ nix/users/ellie.nix
@@ -7,4 +7,15 @@
   home.packages = with pkgs; [
     p7zip-rar
   ];
+
+  users.users."ellie" = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUAs4RQBUriBrp7rv2cepCve5eIo6uqFfgs7oPqV9Q" # 1Password -> 'Primary SSH key'
+    ];
+    isNormalUser = true;
+
+    group = "wheel";
+    shell = pkgs.fish;
+    createHome = true;
+  };
 }
