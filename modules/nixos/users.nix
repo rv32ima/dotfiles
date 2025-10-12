cbb5c03bea38cc827300dcd8c4fe6b34c5ec3eeb
Ellie Ford <me@ellie.fm>
2025-10-12 11:38:43 -0700

    fixup users module

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -5,11 +5,17 @@
   ...
 }:
 lib.mkIf config.rv32ima.machine.enable {
+  imports = builtins.map (user: "${inputs.self}/users/${user}/default.nix") (
+    builtins.attrNames (
+      lib.filterAttrs (_: v: v == "directory") (builtins.readDir "${inputs.self}/users")
+    )
+  );
 
+  config = {
+    users.groups.trusted = { };
+    # users in trusted group are trusted by the nix-daemon
+    nix.settings.trusted-users = [ "@trusted" ];
 
-  users.groups.trusted = { };
-  # users in trusted group are trusted by the nix-daemon
-  nix.settings.trusted-users = [ "@trusted" ];
-
-  users.mutableUsers = false;
+    users.mutableUsers = false;
+  };
 }
