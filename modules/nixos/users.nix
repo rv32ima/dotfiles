0062bc295f88d2018a973cc38fe942ab31cd0e10
Ellie Ford <me@ellie.fm>
2025-10-12 11:39:59 -0700

    >????

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -4,18 +4,21 @@
   inputs,
   ...
 }:
-lib.mkIf config.rv32ima.machine.enable {
-  imports = builtins.map (user: "${inputs.self}/users/${user}/default.nix") (
-    builtins.attrNames (
-      lib.filterAttrs (_: v: v == "directory") (builtins.readDir "${inputs.self}/users")
-    )
-  );
+if config.rv32ima.machine.enable then
+  {
+    imports = builtins.map (user: "${inputs.self}/users/${user}/default.nix") (
+      builtins.attrNames (
+        lib.filterAttrs (_: v: v == "directory") (builtins.readDir "${inputs.self}/users")
+      )
+    );
 
-  config = {
-    users.groups.trusted = { };
-    # users in trusted group are trusted by the nix-daemon
-    nix.settings.trusted-users = [ "@trusted" ];
+    config = {
+      users.groups.trusted = { };
+      # users in trusted group are trusted by the nix-daemon
+      nix.settings.trusted-users = [ "@trusted" ];
 
-    users.mutableUsers = false;
-  };
-}
+      users.mutableUsers = false;
+    };
+  }
+else
+  { }
