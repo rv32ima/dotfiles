c290d0a4e7e23e891a3b69bd034d703334f369c8
Ellie Ford <me@ellie.fm>
2025-10-27 12:38:23 -0700

    oops

--- machines/nixos/unmusique/installer.nix
+++ machines/nixos/unmusique/installer.nix
@@ -29,7 +29,7 @@
     boot.loader.grub.efiInstallAsRemovable = true;
     boot.loader.grub.device = "nodev";
 
-    isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+    # isoImage.squashfsCompression = "gzip -Xcompression-level 1";
     systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUAs4RQBUriBrp7rv2cepCve5eIo6uqFfgs7oPqV9Q" # 1Password -> 'Primary SSH key'
