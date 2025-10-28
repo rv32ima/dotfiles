b83b07b2fb2204f4c740baca802fae856235b08b
Ellie Ford <me@ellie.fm>
2025-10-28 09:10:11 -0700

    zephyr installer

--- machines/nixos/zephyr/installer.nix
+++ machines/nixos/zephyr/installer.nix
@@ -6,7 +6,7 @@
 }:
 {
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/netboot/netboot-minimal.nix"
     ./network.nix
   ];
 
@@ -24,12 +24,12 @@
       inputs.disko.packages.x86_64-linux.default
     ];
 
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.grub.efiSupport = true;
-    boot.loader.grub.efiInstallAsRemovable = true;
-    boot.loader.grub.device = "nodev";
+    # boot.loader.efi.canTouchEfiVariables = true;
+    # boot.loader.grub.efiSupport = true;
+    # boot.loader.grub.efiInstallAsRemovable = true;
+    # boot.loader.grub.device = "nodev";
 
-    isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+    # isoImage.squashfsCompression = "gzip -Xcompression-level 1";
     systemd.services.sshd.wantedBy = lib.mkForce [ "multi-user.target" ];
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUAs4RQBUriBrp7rv2cepCve5eIo6uqFfgs7oPqV9Q" # 1Password -> 'Primary SSH key'
