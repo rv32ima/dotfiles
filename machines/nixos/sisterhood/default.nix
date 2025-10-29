9a3582e8ae7447e4a227dae8c40eb39cf0a49b88
Ellie Ford <me@ellie.fm>
2025-10-28 21:33:19 -0700

    persist /var/lib/plex on sisterhood

--- machines/nixos/sisterhood/default.nix
+++ machines/nixos/sisterhood/default.nix
@@ -18,6 +18,45 @@
     ];
     rv32ima.machine.isRemote = true;
     rv32ima.machine.impermanence.enable = true;
+    rv32ima.machine.impermanence.extraPersistDirectories = [
+      {
+        path = "/var/log";
+        mode = "0644";
+        owner = "root";
+        group = "root";
+      }
+      {
+        path = "/var/lib/nixos";
+        mode = "0644";
+        owner = "root";
+        group = "root";
+      }
+      {
+        path = "/var/lib/systemd/coredump";
+        mode = "0644";
+        owner = "root";
+        group = "root";
+      }
+      {
+        path = "/var/lib/systemd/timers";
+        mode = "0644";
+        owner = "root";
+        group = "root";
+      }
+      {
+        type = "f";
+        path = "/var/lib/tailscale/tailscaled.state";
+        mode = "0600";
+        owner = "root";
+        group = "root";
+      }
+      {
+        path = "/var/lib/plex";
+        mode = "0644";
+        owner = "plex";
+        group = "plex";
+      }
+    ];
 
     services.getty.autologinUser = "root";
 
@@ -50,6 +89,7 @@
 
     networking.useNetworkd = true;
     networking.firewall.allowedTCPPorts = [ ];
+    networking.firewall.logRefusedConnections = false;
 
     services.plex.enable = true;
     services.plex.openFirewall = true;
