7bee4e6fb9d0288f9cff7411a8950d4383891d52
Ellie Ford <me@ellie.fm>
2025-10-29 10:43:31 -0700

    refactor impermanence a little bit

--- modules/nixos/impermanence.nix
+++ modules/nixos/impermanence.nix
@@ -27,6 +27,14 @@
         }
       );
       default = [
+
+      ];
+    };
+  };
+
+  config =
+    let
+      defaultImpermanenceDirs = [
         {
           path = "/var/log";
           mode = "0644";
@@ -51,76 +59,77 @@
           owner = "root";
           group = "root";
         }
-        {
-          path = "/var/lib/tailscale";
-          mode = "0644";
-          owner = "root";
-          group = "root";
-        }
-      ];
-    };
-  };
-
-  config = lib.mkIf config.rv32ima.machine.impermanence.enable {
-    services.openssh.hostKeys = [
-      {
-        path = "/persist/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/persist/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-        bits = 4096;
-      }
-    ];
-
-    systemd.tmpfiles.rules = lib.lists.flatten (
-      [
-        "d /persist/etc/ssh 0644 root root"
       ]
-      ++ (builtins.map (
-        {
-          path,
-          mode,
-          owner,
-          group,
-        }:
-        [
-          "d \"/persist${path}\" ${mode} ${owner} ${group}"
-        ]
-      ) config.rv32ima.machine.impermanence.extraPersistDirectories)
-    );
+      ++ lib.lists.optional (config.services.tailscale.enable) {
+        path = "/var/lib/tailscale";
+        mode = "0644";
+        owner = "root";
+        group = "root";
+      };
 
-    fileSystems = builtins.listToAttrs (
-      builtins.map (
-        { path, ... }:
+      impermanenceDirs =
+        defaultImpermanenceDirs ++ config.rv32ima.machine.impermanence.extraPersistDirectories;
+    in
+    lib.mkIf config.rv32ima.machine.impermanence.enable {
+      services.openssh.hostKeys = [
         {
-          name = path;
-          value = {
-            device = "/persist${path}";
-            options = [ "bind" ];
-          };
+          path = "/persist/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
         }
-      ) config.rv32ima.machine.impermanence.extraPersistDirectories
-    );
+        {
+          path = "/persist/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+          bits = 4096;
+        }
+      ];
 
-    boot.initrd.systemd.enable = true;
-    boot.initrd.systemd.services.zfs-rollback = {
-      enable = true;
-      after = [
-        "zfs-import-zroot.service"
-      ];
-      wantedBy = [
-        "initrd.target"
-      ];
-      before = [
-        "sysroot.mount"
-      ];
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig.Type = "oneshot";
-      script = ''
-        ${pkgs.zfs}/bin/zfs rollback -r zroot/root@blank && echo "zfs rollback complete"
-      '';
+      systemd.tmpfiles.rules = lib.lists.flatten (
+        [
+          "d /persist/etc/ssh 0644 root root"
+        ]
+        ++ (builtins.map (
+          {
+            path,
+            mode,
+            owner,
+            group,
+          }:
+          [
+            "d \"${lib.path.append /persist path}\" ${mode} ${owner} ${group}"
+          ]
+        ) impermanenceDirs)
+      );
+
+      fileSystems = builtins.listToAttrs (
+        builtins.map (
+          { path, ... }:
+          {
+            name = path;
+            value = {
+              device = "${lib.path.append /persist path}";
+              options = [ "bind" ];
+            };
+          }
+        ) impermanenceDirs
+      );
+
+      boot.initrd.systemd.enable = true;
+      boot.initrd.systemd.services.zfs-rollback = {
+        enable = true;
+        after = [
+          "zfs-import-zroot.service"
+        ];
+        wantedBy = [
+          "initrd.target"
+        ];
+        before = [
+          "sysroot.mount"
+        ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          ${pkgs.zfs}/bin/zfs rollback -r zroot/root@blank && echo "zfs rollback complete"
+        '';
+      };
     };
-  };
 }
