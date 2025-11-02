7c032a935ddf5fd436741c1bf1223c17ee7835aa
Ellie Ford <me@ellie.fm>
2025-11-02 10:13:58 -0800

    victoriametrics + tailscalesd

--- machines/nixos/unmusique/default.nix
+++ machines/nixos/unmusique/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   ...
 }:
 {
@@ -21,6 +22,14 @@
     rv32ima.machine.zfsMirrorDisks = [
       "/dev/disk/by-id/ata-MK000480GZXRA_S6M8NE0RC00250"
       "/dev/disk/by-id/ata-MK000480GZXRA_S6M8NE0T100628"
+    ];
+    rv32ima.machine.impermanence.extraPersistDirectories = [
+      {
+        path = /var/lib/victoriametrics;
+        mode = "0770";
+        owner = "victoriametrics";
+        group = "victoriametrics";
+      }
     ];
 
     services.getty.autologinUser = "root";
@@ -54,5 +63,21 @@
 
     networking.useNetworkd = true;
     networking.firewall.allowedTCPPorts = [ ];
+
+    services.victoriametrics.enable = true;
+    services.victoriametrics.retentionPeriod = "30d";
+
+    sops.secrets."services/tailscalesd/environment" = {
+      sopsFile = ./secrets/tailscalesd.yaml;
+    };
+
+    virtualisation.oci-containers.containers."tailscalesd" = {
+      image = "ghcr.io/cfunkhouser/tailscalesd:latest";
+      hostname = "tailscalesd";
+      ports = [ "127.0.0.1:9242:9242" ];
+      environmentFiles = [
+        config.sops.secrets."services/tailscalesd/environment".path
+      ];
+    };
   };
 }
