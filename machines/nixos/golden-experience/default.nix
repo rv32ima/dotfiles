7bfbb99b78e9bc6d1e57cba022fe1e19255d0377
Ellie Ford <me@ellie.fm>
2025-11-08 12:06:50 -0800

    one more fix

--- machines/nixos/golden-experience/default.nix
+++ machines/nixos/golden-experience/default.nix
@@ -19,6 +19,18 @@
     ];
     rv32ima.machine.isRemote = true;
 
+    services.openssh.hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
+
     wsl.enable = true;
     wsl.defaultUser = "ellie";
 
