57bd1a05ea7676ca4b356af5e3c709bb6349ad68
Ellie Ford <me@ellie.fm>
2025-11-21 13:07:48 -0800

    hmmmmm

--- machines/nixos/ghostholding/default.nix
+++ machines/nixos/ghostholding/default.nix
@@ -29,13 +29,12 @@
       "usb_storage"
       "sd_mod"
       "sr_mod"
-      "mlxfw"
-      "mlxreg_io"
-      "mlxreg_hotplug"
-      "mlx_wdt"
     ];
     boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ "kvm-intel" ];
+    boot.kernelModules = [
+      "kvm-intel"
+      "mlxfw"
+    ];
     boot.extraModulePackages = [ ];
     boot.kernelParams = [
       "console=tty0"
