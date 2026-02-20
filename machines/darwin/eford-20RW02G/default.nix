d061584203477854efc42e84a38e59fa8b097f69
Ellie Ford <me@ellie.fm>
2026-02-20 11:46:31 -0800

    machines: add pinterest laptop

---
+++ machines/darwin/eford-20RW02G/default.nix
@@ -0,0 +1,20 @@
+{
+  ...
+}:
+{
+  config = {
+    rv32ima.machine.enable = true;
+    rv32ima.machine.hostName = "eford-20RW02G";
+    rv32ima.machine.stateVersion = 6;
+    rv32ima.machine.primaryUser = "eford";
+    rv32ima.machine.platform = "aarch64-darwin";
+    rv32ima.machine.users = [
+      "eford"
+    ];
+    rv32ima.machine.isRemote = false;
+    rv32ima.machine.workstation.enable = true;
+
+    nix.settings.max-jobs = 10;
+    nix.distributedBuilds = true;
+  };
+}
