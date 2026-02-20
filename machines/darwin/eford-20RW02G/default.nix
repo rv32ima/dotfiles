2c0fd5626ba1da1b8e4bccfe1b2fa65846bba768
Ellie Ford <eford@tvscientific.com>
2026-02-20 14:23:48 -0800

    machines/eford-20RW02G: setup golden-experience as local build machine

--- machines/darwin/eford-20RW02G/default.nix
+++ machines/darwin/eford-20RW02G/default.nix
@@ -15,6 +15,21 @@
     rv32ima.machine.isRemote = false;
     rv32ima.machine.workstation.enable = true;
 
+    nix.buildMachines = [
+      {
+        hostName = "golden-experience.net.ellie.fm";
+        system = "x86_64-linux";
+        sshUser = "nix";
+        publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSU81RGRTZnozbHFiWHR6elZGV0JDSWhubEdiQnozOUs0eW9BR04vRFdlTHYK";
+        sshKey = "/etc/nix/builder_ed25519";
+        maxJobs = 32;
+        protocol = "ssh-ng";
+        supportedFeatures = [
+          "kvm"
+        ];
+      }
+    ];
+
     nix.settings.max-jobs = 10;
     nix.distributedBuilds = true;
   };
