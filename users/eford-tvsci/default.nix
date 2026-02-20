b4337a1d34713e0f99823aebc8e49d3a62121b13
Ellie Ford <me@ellie.fm>
2026-02-20 12:20:32 -0800

    users: fixup eford-pinterest/eford-tvsci

---
+++ users/eford-tvsci/default.nix
@@ -0,0 +1,76 @@
+{
+  pkgs,
+  machine,
+  options,
+  config,
+  lib,
+  inputs,
+  ...
+}:
+let
+  homeDirectory =
+    user:
+    if (lib.hasSuffix "darwin" config.nixpkgs.hostPlatform.config) then
+      "/Users/${user}"
+    else
+      "/home/${user}";
+
+in
+{
+  programs.fish.enable = true;
+  programs.fish.useBabelfish = true;
+
+  users.users."eford" = {
+    shell = pkgs.fish;
+    home = homeDirectory "eford";
+    createHome = true;
+  }
+  // lib.optionalAttrs (builtins.hasAttr "extraGroups" (options.users.users.type.getSubOptions { })) {
+    extraGroups = [
+      "wheel"
+      "trusted"
+    ];
+  }
+  //
+    lib.optionalAttrs (builtins.hasAttr "isNormalUser" (options.users.users.type.getSubOptions { }))
+      {
+        isNormalUser = true;
+      };
+
+  home-manager.users."eford" = {
+    imports = [
+      (
+        if config.rv32ima.machine.isRemote then
+          "${inputs.self}/modules/home-manager/remote.nix"
+        else
+          "${inputs.self}/modules/home-manager/local.nix"
+      )
+    ];
+
+    home.file."bin" = {
+      source = "${inputs.self}/bin";
+      recursive = true;
+    };
+
+    home.sessionPath = [
+      "${homeDirectory "eford"}/bin"
+    ];
+
+    home.username = "eford";
+    home.stateVersion = "25.05";
+    home.packages = with pkgs; [
+      zigpkgs."0.15.1"
+      inputs.zls.packages.${system}.default
+      cargo-mommy
+
+      duckdb
+      google-cloud-sdk
+      awscli2
+      rclone
+
+      tenv
+      nix-your-shell
+    ];
+  };
+
+}
