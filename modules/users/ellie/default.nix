2f651ac2c96d1aef16a0d5e5a5ec2e0f334808e6
Ellie Ford <eford@pinterest.com>
2026-03-25 12:29:09 -0700

    [users/ellie]: add new ssh key

--- modules/users/ellie/default.nix
+++ modules/users/ellie/default.nix
@@ -29,6 +29,7 @@ in
   users.users."ellie" = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUAs4RQBUriBrp7rv2cepCve5eIo6uqFfgs7oPqV9Q" # 1Password -> 'Primary SSH key'
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJtlo9dfoKwUfxp4IabM/9IpBHurAVGGAajY6sCzShmBAAAABHNzaDo= eford@eford-20RW02G" # Pinterest Laptop Yubikey
     ];
     shell = pkgs.fish;
     home = homeDirectory "ellie";
