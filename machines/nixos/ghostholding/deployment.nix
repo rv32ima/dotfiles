795e37b86a000a39f0bc6ada201f37963caf5958
Ellie Ford <me@ellie.fm>
2026-04-21 21:54:40 -0700

    machines/nixos: cleanup

--- machines/nixos/ghostholding/deployment.nix
+++
@@ -1,5 +0,0 @@
-{
-  targetHost = "ghostholding.tail09d5b.ts.net";
-  targetPort = 22;
-  targetUser = "root";
-}
--- machines/nixos/golden-experience/deployment.nix
+++
@@ -1,5 +0,0 @@
-{
-  targetHost = "silver-chariot.tail09d5b.ts.net";
-  targetPort = 22;
-  targetUser = "root";
-}
