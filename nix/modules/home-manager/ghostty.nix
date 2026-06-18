d6f9d8a8293a5dd565aa7616635f19bd9ad1c504
Ellie Ford <me@ellie.fm>
2026-06-18 15:58:32 -0700

    ghostty: add smear cursor

--- nix/modules/home-manager/ghostty.nix
+++ nix/modules/home-manager/ghostty.nix
@@ -5,8 +5,8 @@
   ];
 
   home-manager.users."${config.system.primaryUser}" = { config, ... }: {
-    home.file.".config/ghostty/config" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/ghostty/config";
+    home.file.".config/ghostty" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/ghostty";
       recursive = true;
     };
   };
