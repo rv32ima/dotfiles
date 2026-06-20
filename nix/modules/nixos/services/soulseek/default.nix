b8b3e43c3171731d1e2f825f9f03fbd8843b3c90
Ellie Ford <me@ellie.fm>
2026-06-20 14:34:21 -0700

    soulseek: add a cute description

--- nix/modules/nixos/services/soulseek/default.nix
+++ nix/modules/nixos/services/soulseek/default.nix
@@ -17,6 +17,30 @@
     services.slskd.enable = true;
     services.slskd.openFirewall = true;
     services.slskd.settings = {
+      soulseek.description = ''
+        A TRANS PERSON PEED HERE!!
+
+        The Industrial Revolution and its consequences have been a disaster for the human race.
+        They have greatly increased the life-expectancy of those of us who live in “advanced” countries,
+        but they have destabilized society, have made life unfulfilling, have subjected human beings to indignities,
+        have led to widespread psychological suffering (in the Third World to physical suffering as well) and have inflicted severe damage on the natural world.
+        The continued development of technology will worsen the situation.
+        It will certainly subject human beings to greater indignities and inflict greater damage on the natural world,
+        it will probably lead to greater social disruption and psychological suffering,
+        and it may lead to increased physical suffering even in “advanced” countries.
+
+        catgirl infrastructure witch
+        proud polyam anarchist tranny
+        militant nixos user
+        served off of AS395388 (t4t.net) with a dual 25GbE connection
+        source is Bandcamp / Tidal / other Soulseek users
+
+        !! this system is headless and unmonitored. chats will likely go ignored. email me at soulseek@t4t.net instead !!
+
+        xoxo,
+        kitty
+      '';
+      soulseek.picture = ./picture.jpg;
       shares.directories = [
         "/media/music"
       ];
