990f011198bd0eb847eb94745aaa87b9c72ebb0d
Ellie Ford <me@ellie.fm>
2026-06-21 11:58:18 -0700

    lidarr: init

---
+++ nix/modules/nixos/services/lidarr.nix
@@ -0,0 +1,18 @@
+{ ... }:
+{
+  config = {
+    rv32ima.machine.impermanence.extraPersistDirectories = [
+      {
+        path = /var/lib/lidarr/.config/Lidarr;
+        mode = "0770";
+        owner = "lidarr";
+        group = "lidarr";
+      }
+    ];
+
+    services.lidarr.enable = true;
+    rv32ima.machine.tailscale.services.radarr = {
+      port = 8686;
+    };
+  };
+}
