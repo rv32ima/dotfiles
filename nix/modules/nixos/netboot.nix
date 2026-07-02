ed142f51d03aaf4d0e0a1842b491b69d99807ab8
kitty <me@ellie.fm>
2026-07-01 18:05:38 -0700

    netboot: switch back to symlinkJoin (#8)

--- nix/modules/nixos/netboot.nix
+++ nix/modules/nixos/netboot.nix
@@ -9,9 +9,12 @@
     "${modulesPath}/installer/netboot/netboot-minimal.nix"
   ];
 
-  config.system.build.netboot =
-    let
-      autoexec = pkgs.writeTextDir "autoexec.ipxe" ''
+  config.system.build.netboot = pkgs.symlinkJoin {
+    name = "netboot";
+    paths = with config.system.build; [
+      netbootRamdisk
+      kernel
+      (pkgs.writeTextDir "autoexec.ipxe" ''
         #!ipxe
         # Use the cmdline variable to allow the user to specify custom kernel params
         # when chainloading this script from other iPXE scripts like netboot.xyz
@@ -20,16 +23,8 @@
         initrd initrd
         imgargs bzImage init=${config.system.build.toplevel}/init initrd=initrd nohibernate loglevel=4 lsm=landlock,yama,bpf
         boot
-      '';
-    in
-    pkgs.stdenv.mkDerivation {
-      name = "netboot";
-      phases = [ "installPhase" ];
-      installPhase = ''
-        mkdir -p $out
-        cp ${config.system.build.netbootRamdisk}/* $out
-        cp ${config.system.build.kernel}/* $out
-        cp ${autoexec}/* $out
-      '';
-    };
+      '')
+    ];
+    preferLocalBuild = true;
+  };
 }
