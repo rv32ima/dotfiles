81f3b4ce87c090e60dda049f2d0233094b97bffb
Ellie Ford <me@ellie.fm>
2026-07-01 21:08:20 -0700

    vars: add machineId for all gitops machines

--- nix/vars/machines.nix
+++ nix/vars/machines.nix
@@ -13,6 +13,7 @@
   "ghostholding" = {
     system = "x86_64-linux";
     stateVersion = "25.11";
+    machineId = "897f39ed1483490a86add609cc4570bb";
     sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJOdJCK9bK++zCrAqJ5qkvakYMZbcWKynbaWo4F30Jk";
     deployment = {
       targetHost = "ghostholding.tail09d5b.ts.net";
@@ -39,19 +40,10 @@
       ];
     };
   };
-  "sisterhood" = {
-    system = "x86_64-linux";
-    stateVersion = "25.11";
-    sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/aB3u9QTM6k4LnFNr93GdIuu1jQMtvZ58BbmwvWoDg";
-    deployment = {
-      targetHost = "sisterhood.tail09d5b.ts.net";
-      targetPort = 22;
-      targetUser = "root";
-    };
-  };
   "peer2peer" = {
     system = "x86_64-linux";
     stateVersion = "25.11";
+    machineId = "b355711b63fb4686aad0e0412556e6c4";
     sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPz54HjkBeZLYPQMrIaKxl5UmIPcNbHh8L3kNmIgiVRx";
     deployment = {
       targetHost = "peer2peer.tail09d5b.ts.net";
@@ -71,6 +63,7 @@
   "pawpatch" = {
     system = "x86_64-linux";
     stateVersion = "26.05";
+    machineId = "0032bea4f490416188be462cf964e544";
     sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPBFwz8SD8+Yg6BYpd1geNVUsTrFSpm9tVX1dpQtDrw9";
     deployment = {
       targetHost = "pawpatch.tail09d5b.ts.net";
@@ -81,6 +74,7 @@
   "psychoboost" = {
     system = "x86_64-linux";
     stateVersion = "26.05";
+    machineId = "38c3607e2f1d4fd5ad52424acd68cdff";
     sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQP5dUoKULsDpiI73oZabny2hb0Cxw37Qfnnh7pM8QU";
     deployment = {
       targetHost = "psychoboost.tail09d5b.ts.net";
@@ -119,6 +113,7 @@
   "fadeoutz" = {
     system = "x86_64-linux";
     stateVersion = "26.05";
+    machineId = "d79d558458594324a0626831b6fd86b1";
     sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeEX3/9e5OfsCkPw2P/hWN+tLniuNO+muL9Q9KgfJFq";
     deployment = {
       targetHost = "fadeoutz.tail09d5b.ts.net";
