bb27bea33f3b5093a8b499b5797cc0550ba9fd02
Ellie Ford <me@ellie.fm>
2026-07-03 15:19:06 -0700

    nixos/deploy: add 1Password ssh key

--- nix/modules/nixos/deploy.nix
+++ nix/modules/nixos/deploy.nix
@@ -3,7 +3,8 @@
     users.users.deploy = {
       group = "deploy";
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOovYff9edwaEZfqoRz1VQgiJ3wdCDpebxj5fzHIrFSW"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOovYff9edwaEZfqoRz1VQgiJ3wdCDpebxj5fzHIrFSW" # CI deploy key
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUAs4RQBUriBrp7rv2cepCve5eIo6uqFfgs7oPqV9Q" # 1Password -> 'Primary SSH key'
       ];
       shell = pkgs.bashInteractive;
       isSystemUser = true;
