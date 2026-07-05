06c9062bc59df8f901620ad1ed8c60e9f9e7709e
Ellie Ford <me@ellie.fm>
2026-07-05 11:40:15 -0700

    nuke nix folder, since that's moved to t4t-net/infra

--- nix/machines/nixos/ghostholding/README.md
+++
@@ -1,6 +0,0 @@
-# ghostholding
-
-## Keyboard Shortcuts To Know:
-* Esc + R, Esc + r, Esc + R: Reset button
-* Ctrl+a, Ctrl+b: SysRq
-* Ctrl+b: Enter BIOS
--- nix/modules/nixos/services/prowlarr.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  config = {
-    services.prowlarr.enable = true;
-    rv32ima.machine.tailscale.services.prowlarr = {
-      port = 9696;
-    };
-  };
-}
